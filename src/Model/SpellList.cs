4c8ed06c3f88f1bf9b5fb632022316eb4075acff
Jelinder34 <jelinder34@yahoo.com>
2019-03-21 15:35:51 -0400

    Updated SpellList.cs

--- src/Model/SpellList.cs
+++ src/Model/SpellList.cs
@@ -1,103 +1,42 @@
 ﻿
 namespace C246SpellBook_V_2
 {
-    class SpellList
+    public class SpellList
     {
-            private string id;
-            private string name;
-            private string level;
-            private string school;
-            private bool ritual;
-            private bool concentration;
-            private string time;
-            private string range;
-            private string components;
-            private string materials;
-            private string duration;
-            private string classes;
-            private string description;
-            private string higherLevel;
-            private string source;
-            
 
-            public SpellList(string id, string name, string level, string school, bool ritual, bool concentration, string time, string range, string components, string materials, string duration, string classes, string description, string higherLevel, string source)
+        private SpellList(string id, string name, string level, string school, bool ritual, bool concentration, string time, string range, string components, string materials, string duration, string classes, string description, string higherLevel, string source)
             {
-                this.id = id;
-                this.name = name;
-                this.level = level;
-                this.school = school;
-                this.ritual = ritual;
-                this.concentration = concentration;
-                this.time = time;
-                this.range = range;
-                this.components = components;
-                this.materials = materials;
-                this.duration = duration;
-                this.classes = classes;
-                this.description = description;
-                this.higherLevel = higherLevel;
-                this.source = source;
+                ID = id;
+                Name = name;
+                Level = level;
+                School = school;
+                Ritual = ritual;
+                Concentration = concentration;
+                Time = time;
+                Range = range;
+                Components = components;
+                Materials = materials;
+                Duration = duration;
+                Classes = classes;
+                Description = description;
+                HigherLevel = higherLevel;
+                Source = source;
             }
 
-            public string ID
-            {
-                get { return id; }
-            }
-            public string Name
-            {
-                get { return name; }
-            }
-            public string Level
-            {
-                get { return level; }
-            }
-            public string School
-            {
-                get { return school; }
-            }
-            public bool Ritual
-            {
-                get { return ritual; }
-            }
-            public bool Concentration
-            {
-                get { return concentration; }
-            }
-            public string Time
-            {
-                get { return time; }
-            }
-            public string Range
-            {
-                get { return range; }
-            }
-            public string Components
-            {
-                get { return components; }
-            }
-            public string Materials
-            {
-                get { return materials; }
-            }
-            public string Duration
-            {
-                get { return duration; }
-            }
-            public string Classes
-            {
-                get { return classes; }
-            }
-            public string Description
-            {
-                get { return description; }
-            }
-            public string HigherLevel
-            {
-                get { return higherLevel; }
-            }
-            public string Source
-            {
-                get { return source; }
-            }
+        private string ID { get; set; }
+        private string Name { get; set; }
+        private string Level { get; set; }
+        private string School { get; set; }
+        private bool Ritual { get; set; }
+        private bool Concentration { get; set; }
+        private string Time { get; set; }
+        private string Range { get; set; }
+        private string Components { get; set; }
+        private string Materials { get; set; }
+        private string Duration { get; set; }
+        private string Classes { get; set; }
+        private string Description { get; set; }
+        private string HigherLevel { get; set; }
+        private string Source { get; set; }
     }
 }
